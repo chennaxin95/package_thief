2658a92290d5fdf4a09bdc6c43130ff6319d64ac
Naxin Chen <nc352@cornell.edu>
2018-04-13 20:59:25 -0400

    video handling

---
+++ video_handler.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+
+def read_video(file_path):
+    cap = cv2.VideoCapture(file_path)
+    frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    buf = np.empty((frameCount, frameHeight, frameWidth, 3), np.dtype('uint8'))
+
+    return buf
+
+def 
