f2a39425fad3beee7e832700050948ca3aa5f817
Naxin Chen <nc352@cornell.edu>
2018-04-23 00:29:26 -0400

    uploading backend for image acception

--- back_end.py
+++ back_end.py
@@ -1,11 +1,16 @@
 import hug
 
 import cv2
+import json
 import numpy as np
 
-@hug.post('/uploadIMG')
-def take_img(cli_id, img):
-    cli_id = int(cli_id)
-    np_img = np.fromstring(img)
-    _, w, h = np_img.shape
-    print("got image size of {} x {}".format(w, h)) 
+@hug.post('/img_upload')
+def take_img( body):
+    # cli_id = int(cli_id)
+    img = body['img']
+    np_img = np.frombuffer(img, dtype=np.int8)
+    info = json.loads(body['info'].decode('utf8'))
+    shape = info['size']
+    print(info['id'])
+    np_img = np_img.reshape(shape)
+    print(np_img.shape)
