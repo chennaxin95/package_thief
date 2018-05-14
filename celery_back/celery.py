ddca6dd44f45029cc08c61af93cd5d5b62cf6722
Naxin Chen <nc352@cornell.edu>
2018-05-14 18:26:13 -0400

    change serializer

--- celery_back/celery.py
+++ celery_back/celery.py
@@ -2,5 +2,8 @@ from celery import Celery
 
 app = Celery('celery_back', borker='redis://package_thief@localhost:6379')
 
+app.conf.task_serializer = 'pickle'
+app.conf.result_serializer = 'pickle'
+
 if __name__=='__main__':
     app.start()
