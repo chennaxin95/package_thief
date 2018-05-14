03c77c4e3d09c1234039baf54e4c20cb18df4318
Yiwei Ni <anitani313@gmail.com>
2018-05-14 18:31:52 -0400

    changing serializer

--- celery_back/celery.py
+++ celery_back/celery.py
@@ -1,6 +1,7 @@
 from celery import Celery
 
-app = Celery('celery_back', borker='redis://package_thief@localhost:6379')
+app = Celery('celery_back', borker='redis://package_thief@localhost:6379',
+            task_serializer='pickle', result_serializer='pickle')
 
 app.conf.task_serializer = 'pickle'
 app.conf.result_serializer = 'pickle'
